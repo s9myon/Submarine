e467f3c0628923859eabdf03e34966c203fed82d
Bolt98 <fuskerr63@gmail.com>
2020-03-25 19:52:51 +0400

    delete public1

--- public/src/helpers/socket.js
+++
@@ -1,6 +0,0 @@
-import openSocket from 'socket.io-client';
-
-// создаём используемый сокет и экспортируем его для дальнейшего использования
-const socket = openSocket('http://localhost:9000');
-
-export default socket;
