f3deee7e6eb0fbce87d28264d368cf5ff9f04790
Andrey <53421644+aiipo@users.noreply.github.com>
2020-03-29 10:30:10 +0400

    feat: компонента GameManager, реализовано начало игры (#2)

--- application/modules/gameManager/GameManager.js
+++ application/modules/gameManager/GameManager.js
@@ -15,7 +15,7 @@ class GameManager extends BaseManager {
             const team = this.mediator.get(this.TRIGGERS.GET_TEAM, user.id);
             if (user.name === team.name) { // если капитан команды (название команды - id командира)
                 let roomId = this.mediator.get(this.TRIGGERS.GET_ROOMID_BY_USERID, user.id);
-                this.io.in(roomId).emit(this.MESSAGES.START_GAME, true); // отправить сообщение о начале игры команде
+                return this.io.in(roomId).emit(this.MESSAGES.START_GAME, true); // отправить сообщение о начале игры команде
             }
         }
         socket.emit(this.MESSAGES.START_GAME, false);
