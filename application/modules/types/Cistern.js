74bb6df2e2a9df0e7ba157835c7eeb916648aca3
YourOlDBoy <46051099+YourOlDBoy@users.noreply.github.com>
2020-04-01 14:32:23 +0400

    Add files via upload

---
+++ application/modules/types/Cistern.js
@@ -0,0 +1,9 @@
+class Cistern extends Equipment {
+    constructor(){
+        super();
+    }
+
+    fill(){}// Наполнить
+    empty(){}//опорожнить
+}
+module.exports = Cistern;
---
+++ application/modules/types/Torpedo.js
@@ -0,0 +1,10 @@
+class Torpedo extends Equipment {
+    constructor (){
+        super();
+        this.speed = null;
+        this.angle = null;
+        this.deep = null;
+    }
+    load(){};
+    fire(){};
+}
